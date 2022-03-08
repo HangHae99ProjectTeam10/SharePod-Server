a72e5f9488ca190a49e266649c239bb2231991d5
leeseungsoo <seungsoo0701@gmail.com>
2022-03-08 15:22:33 +0900

    AS-IS ) RDS Mysql 연동 TO-BE ) 엔터티 구성 완료 및 테스트

---
+++ src/main/java/com/spring/sharepod/SharepodApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sharepod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharepodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharepodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/sharepod/SharepodApplicationTests.java
@@ -0,0 +1,28 @@
+package com.spring.sharepod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharepodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    void Test(){
+
+    }
+
+    @Test
+    void thridTest(){
+
+    }
+
+    @Test
+    void UpdateToTest(){
+
+    }
+}
