2db7b646cfb013c36c9f74cf9334623c1c1ba0a6
leeseungsoo <seungsoo0701@gmail.com>
2022-03-11 20:56:39 +0900

    AS-IS) NoticeController, NoticeService 생성 성공 TO-BE) 알림 구현

---
+++ src/main/java/com/spring/sharepod/service/NoticeService.java
@@ -0,0 +1,10 @@
+package com.spring.sharepod.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+}
