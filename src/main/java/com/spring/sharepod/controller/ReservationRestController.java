913318b883b08633f0532febd87ac386aeb748b1
thdals83 <thdals83@naver.com>
2022-03-11 18:38:05 +0900

    AS-IS ) 거래요청 GET, Reservation, Auth, User 엔티티 수정 TO-BE ) 거래요청 수락 거절 구현

--- src/main/java/com/spring/sharepod/controller/ReservationRestController.java
+++ src/main/java/com/spring/sharepod/controller/ReservationRestController.java
@@ -18,6 +18,9 @@ public class ReservationRestController {
         return reservationService.reserRequestService(boardid,requestDTO);
     }
 
+
+
+
     //거래 요청 목록(현재 접속한 사람에게 온 요청 목록)
     @GetMapping("/reservation/{userid}")
     public ReservationGetFinalDTO reservationGetControll(@PathVariable Long userid){
