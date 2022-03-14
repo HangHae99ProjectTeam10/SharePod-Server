15e0910a87d7f3ded004d165dc73ea9d07de3e1f
leeseungsoo <seungsoo0701@gmail.com>
2022-03-14 14:32:09 +0900

    AS-IS) 인증이 모두 끝난 후의 재업로드 혹은 게시글 삭제 구현 완료(케스케이드 확인해야함) TO-BE) 로그인 탈퇴 구현

--- src/main/java/com/spring/sharepod/repository/AuthRepository.java
+++ src/main/java/com/spring/sharepod/repository/AuthRepository.java
@@ -1,8 +1,11 @@
 package com.spring.sharepod.repository;
 
 import com.spring.sharepod.entity.Auth;
+import com.spring.sharepod.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +15,12 @@ public interface AuthRepository extends JpaRepository<Auth, Long> {
 
     @Query("select a from Auth a where a.authseller.id=:sellerid")
     List<Auth> findBySellerId(Long sellerid);
+
+    @Query("select a.board from Auth a where a.id=:authid")
+    Board findBoardByAuthId(Long authid);
+
+    @Modifying
+    @Transactional
+    @Query("delete from Board b where b.id=:boardid")
+    void deleteByBoardId(Long boardid);
 }
--- src/main/java/com/spring/sharepod/repository/AuthimgboxRepository.java
+++ src/main/java/com/spring/sharepod/repository/AuthimgboxRepository.java
@@ -2,6 +2,15 @@ package com.spring.sharepod.repository;
 
 import com.spring.sharepod.entity.Authimgbox;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AuthimgboxRepository extends JpaRepository<Authimgbox, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("delete from Authimgbox a where a.auth.id=:authid")
+    void deleteByAuthId(Long authid);
+
 }
