8e20f8a1dff19f3890d4e8839877944e8b457169
thdals83 <thdals83@naver.com>
2022-03-20 21:16:44 +0900

    AS-IS ) api 수정 TO-BE ) 1:1 채팅 구현

--- src/main/java/com/spring/sharepod/v1/dto/response/BoardResponseDto.java
+++ src/main/java/com/spring/sharepod/v1/dto/response/BoardResponseDto.java
@@ -13,6 +13,32 @@ public class BoardResponseDto {
     @Getter
     @AllArgsConstructor
     @Builder
+    public static class BoardWrite {
+        private String result;
+        private String msg;
+        private BoardData boardData;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class BoardData {
+
+        private Long boardId;
+        private String firstImgUrl;
+        private String title;
+        private String boardRegion;
+        private int dailyRentalFee;
+        private String boardTag;
+        private String category;
+        private LocalDateTime modifiedAt;
+
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
     public static class BoardAll {
 
         private Long boardId;
