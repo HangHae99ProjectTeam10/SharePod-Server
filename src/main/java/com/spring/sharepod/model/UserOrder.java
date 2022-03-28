27138053b7c7070f9746f39445f4189069797785
leeseungsoo <seungsoo0701@gmail.com>
2022-03-28 09:47:18 +0900

    AS-IS) 기존 마이페이지 API 병합 및 18번 API querydsl 적용 TO-BE) 19번 API querydsl 적용

---
+++ src/main/java/com/spring/sharepod/model/UserOrder.java
@@ -0,0 +1,20 @@
+package com.spring.sharepod.model;
+
+import com.spring.sharepod.v1.dto.response.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserOrder {
+    private String result;
+    private String msg;
+    private List<RentBuyer> rentBuyerList;
+    private List<RentSeller> rentSellerList;
+    private List<UserReservation> userReservationList;
+
+}
