0d322387a38644244d16592581885a1b72b85472
thdals83 <thdals83@naver.com>
2022-03-29 15:03:03 +0900

    AS-IS) 폴더 구조 변경 TO-BE) 리팩토링

--- src/main/java/com/spring/sharepod/v1/dto/response/notice/NoticeInfoResponseDto.java
+++ src/main/java/com/spring/sharepod/v1/dto/response/notice/NoticeInfoResponseDto.java
@@ -1,6 +1,5 @@
 package com.spring.sharepod.v1.dto.response.notice;
 
-import com.spring.sharepod.v1.dto.response.notice.Notice;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
