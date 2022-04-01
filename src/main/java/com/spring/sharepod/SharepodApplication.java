8a2b1c6d4d91cc9b4958c6a6f68a62375a34eb99
thdals83 <thdals83@naver.com>
2022-04-01 18:41:45 +0900

    AS-IS) 채팅 수정 TO-BE) 리팩토링

--- src/main/java/com/spring/sharepod/SharepodApplication.java
+++ src/main/java/com/spring/sharepod/SharepodApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing //시간 자동 변경 가능하게
+@EnableScheduling // 특정 시간에 발동됨
 @SpringBootApplication //스프링 부트 어플
 public class SharepodApplication {
 
